3ac78b57396fba4fae4704dc1d0d653ec3f6a017
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2019-11-21 13:52:31 +0900

    Add sign in/out function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :move_to_sign_in, only: :index
+  
   def index
+  end
+
+  private
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
