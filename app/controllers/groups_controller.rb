ae7b214ccc9dc9d8860d087507cec409e9fc2be0
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2019-11-22 22:04:46 +0900

    Show user's groups on index page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,16 @@
 class GroupsController < ApplicationController
+  before_action :move_to_sign_in, only: :index
+  
+  def index
+    @groups = current_user.groups
+  end
+
   def new
     @group = Group.new
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = Group.new(group_name)
     if @group.save
       @group.update(group_users)
       flash[:notice] = "グループを作成しました"
@@ -15,18 +21,37 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:notice] = "グループを編集しました"
+      redirect_to root_path
+    else
+      render action: :edit
+    end
   end
 
   private
     def group_params
+      params.require(:group).permit(:name, {user_ids: []})
+    end
+
+    def group_name
       params.require(:group).permit(:name)
     end
 
     def group_users
       params.require(:group).permit(user_ids: [])
+    end
+
+    def move_to_sign_in
+      unless user_signed_in?
+        flash[:alert] = "ログインまたは登録が必要です。"
+        redirect_to new_user_session_path
+      end
     end
 
 end
