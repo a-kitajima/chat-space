8d964bf3dd61ad00188cad968a9db98f6dd84b47
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2019-11-23 17:43:35 +0900

    Resize image

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,9 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     unless @message.body.empty? && @message.image.file.nil?
+      if @message.image.file
+        resize_image(@message.image)
+      end
       @message.save
     else
       flash[:alert] = "メッセージを入力してください。"
@@ -19,5 +22,12 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def resize_image(image)
+    require 'mini_magick'
+    resize_image = MiniMagick::Image.open(image.current_path)
+    resize_image.resize "200x200"
+    resize_image.write image.current_path
   end
 end
