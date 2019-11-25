3fba01f8239f9ec2f01308c3195434202316df28
a-kitajima <57699446+a-kitajima@users.noreply.github.com>
2019-11-25 13:48:22 +0900

    Add test for Message model

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
   factory :message do
     body      {"testtext"}
     image     {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/testimage.png'))}
-    group_id  {1}
-    user_id   {1}
+    group
+    user
   end
 
 end
